03363557784fae7acac1e577605b1649bccf6330
Ron Shortt <ron@tp500xu1.shortt.cloud>
2023-06-22 13:26:30 -0400

    Change bg to black.

--- nvim/.config/nvim/lua/core/plugin_config/gruvbox.lua
+++ nvim/.config/nvim/lua/core/plugin_config/gruvbox.lua
@@ -1,2 +1,9 @@
 vim.o.termguicolors = true
+vim.o.background = "dark"
+require("gruvbox").setup({
+    contrast = "hard",
+    palette_overrides = {
+        dark0_hard = "#000000",
+    }
+})
 vim.cmd [[ colorscheme gruvbox ]]
